1d1bf6472bd9367e775edcdd3df9315085ea8ea9
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2025-02-09 11:39:59 +0000

    improve python script, add config arg

--- cloudflare/cloudflare-update-dns.py
+++ cloudflare/cloudflare-update-dns.py
@@ -7,6 +7,11 @@ CLOUDFLARE_API_URL = 'https://api.cloudflare.com/client/v4'
 LOG_LEVELS = ['none', 'info', 'debug']
 LOG_LEVEL = 'none'
 
+ZONE_ID = ""
+RECORD_NAME = ""
+API_KEY = ""
+EMAIL = ""
+
 # logging
 def log(message, level='info'):
     if level not in LOG_LEVELS:
@@ -16,13 +21,13 @@ def log(message, level='info'):
         print(f'[{level}] {message}')
 
 # get cloudlflare records list
-def get_cloudflare_records(zone_id, record_name, api_key, email):
+def get_cloudflare_records():
     """
     Retrieves the Cloudflare DNS record ID for a given zone and record name.
 
     Args:
         zone_id (str): The ID of the Cloudflare zone.
-        record_name (str): The name of the DNS record to retrieve.
+        RECORD_NAME (str): The name of the DNS record to retrieve.
         api_key (str): The Cloudflare API key.
         email (str): The email associated with the Cloudflare account.
 
@@ -35,8 +40,8 @@ def get_cloudflare_records(zone_id, record_name, api_key, email):
     res = None
     try:
         res = requests.get(
-            f'{CLOUDFLARE_API_URL}/zones/{zone_id}/dns_records',
-            headers={'X-Auth-Email': email, 'X-Auth-Key': api_key}
+            f'{CLOUDFLARE_API_URL}/zones/{ZONE_ID}/dns_records',
+            headers={'X-Auth-Email': EMAIL, 'X-Auth-Key': API_KEY}
         )
         res.raise_for_status()
         if res.status_code != 200:
@@ -50,21 +55,21 @@ def get_cloudflare_records(zone_id, record_name, api_key, email):
 
     requested_record_ids = ''
     for record in res['result']:
-        if record['name'] == record_name:
+        if record['name'] == RECORD_NAME:
             requested_record_ids = record['id']
 
     if not requested_record_ids:
-        raise ConnectionError(f"Error fetching Cloudflare records: Record with name {record_name} not found")
+        raise ConnectionError(f"Error fetching Cloudflare records: Record with name {RECORD_NAME} not found")
 
     return requested_record_ids
 
 # update cloudflare record
-def update_cloudflare_record(record_name, record_id, zone_id, current_ip, api_key, email):
+def update_cloudflare_record(record_id, current_ip):
     """
     Updates a Cloudflare DNS record with the current IP address.
 
     Args:
-        record_name (str): The name of the DNS record to update.
+        RECORD_NAME (str): The name of the DNS record to update.
         record_id (str): The ID of the DNS record to update.
         zone_id (str): The ID of the Cloudflare zone.
         current_ip (str): The current public IP address.
@@ -75,16 +80,16 @@ def update_cloudflare_record(record_name, record_id, zone_id, current_ip, api_ke
         ConnectionError: If there is an error updating the Cloudflare record.
     """
 
-    url = f'{CLOUDFLARE_API_URL}/zones/{zone_id}/dns_records/{record_id}'
+    url = f'{CLOUDFLARE_API_URL}/zones/{ZONE_ID}/dns_records/{record_id}'
     headers = {
-        'X-Auth-Email': email,
-        'X-Auth-Key': api_key,
+        'X-Auth-Email': EMAIL,
+        'X-Auth-Key': API_KEY,
         'content-type': 'application/json'
     }
     data = {
       "comment": "Domain verification record",
       "content": current_ip,
-      "name": record_name,
+      "name": RECORD_NAME,
       "proxied": True,
       "ttl": 600,
       "type": "A"
@@ -133,24 +138,39 @@ def main():
     Main function to update Cloudflare DNS record with the current public IP address.
 
     This function performs the following steps:
-    1. Loads environment variables from a .env file.
-    2. Retrieves the required environment variables: ZONE_ID, RECORD_NAME, API_KEY, EMAIL.
-    3. Checks if all required environment variables are present.
-    4. Gets the Cloudflare record ID for the specified zone and record name.
-    5. Retrieves the current public IP address.
-    6. Updates the Cloudflare DNS record with the current public IP address.
+    1. Gets the Cloudflare record ID for the specified zone and record name.
+    2. Retrieves the current public IP address.
+    3. Updates the Cloudflare DNS record with the current public IP address.
 
     Raises:
         EnvironmentError: If any of the required environment variables are missing.
     """
-    # get env vars from .env file
-    load_dotenv()
-    zone_id = os.getenv('ZONE_ID')
-    record_name = os.getenv('RECORD_NAME')
-    api_key = os.getenv('API_KEY')
-    email = os.getenv('EMAIL')
 
-    if not all([zone_id, record_name, api_key, email]):
+    record_id = get_cloudflare_records()
+    current_ip = get_public_ip()
+    update_cloudflare_record(record_id, current_ip)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Update Cloudflare DNS record with current public IP')
+    parser.add_argument('--log-level', type=str, default='none', help='Log level: info or debug')
+    parser.add_argument('--config', type=str, default='', help='Path to .env file')
+    args = parser.parse_args()
+
+    LOG_LEVEL = args.log_level
+    if (args.config):
+        load_dotenv(dotenv_path=args.config)
+    else:
+        load_dotenv()
+
+    # get env vars from .env file
+    ZONE_ID = os.getenv('ZONE_ID')
+    RECORD_NAME = os.getenv('RECORD_NAME')
+    API_KEY = os.getenv('API_KEY')
+    EMAIL = os.getenv('EMAIL')
+
+    if not all([ZONE_ID, RECORD_NAME, API_KEY, EMAIL]):
         raise EnvironmentError("""Missing required environment variables.
 
 Please create a .env file in the root of the project with the following variables:
@@ -159,17 +179,5 @@ RECORD_NAME=your_record_name
 API_KEY=your_api_key
 EMAIL=your_email
 """)
-
-    record_id = get_cloudflare_records(zone_id, record_name, api_key, email)
-    current_ip = get_public_ip()
-    update_cloudflare_record(record_name, record_id, zone_id, current_ip, api_key, email)
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Update Cloudflare DNS record with current public IP')
-    parser.add_argument('--log-level', type=str, default='none', help='Log level: info or debug')
-    args = parser.parse_args()
-
-    LOG_LEVEL = args.log_level
 
     main()
